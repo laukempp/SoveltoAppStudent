056613ac535fcc1f293a200849c4aecd11aa187e
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2019-12-16 15:48:19 +0200

    QuizForm

--- soveltoapp_reactfront/src/components/teacher/Dashboard.js
+++ soveltoapp_reactfront/src/components/teacher/Dashboard.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import auth from '../../service/Auth';
+import Quizform from './Quizform'
 
 export default function Dashboard() {
-  const authT = auth.sessionStorageGetItem();
+  /*const authT = auth.sessionStorageGetItem();
   
   console.log("hello from dashboard", authT);
+  {authT ? null : <Redirect to="/" />}*/
 
   return (
     <div>
-      {authT ? null : <Redirect to="/" />}
+      
       <h1>Tämä on dashboard</h1>
+      <Quizform/>
     </div>
   );
 }
--- soveltoapp_reactfront/src/service/Request.js
+++ soveltoapp_reactfront/src/service/Request.js
@@ -1,3 +1,17 @@
+const token = sessionStorage.getItem('tommi');
+
+export const fetchQuestions =(topic_id)=> {
+  return fetch("api/topics/" + topic_id, {
+    headers: {
+      'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6IlN1bGFra2FAc2lsYWtrYS5maSIsImlkIjoxLCJ0aW1lIjoiMjAxOS0xMi0xNlQxMTo0Mzo1OS43NThaIiwiaWF0IjoxNTc2NDk2NjM5LCJleHAiOjE1NzY1MTgyMzl9.DDL727Zu6GY_k0gWayPr0i42abIoVDRYdExl9pJ72BM',
+    }
+  })
+  .then(res => res.json())
+};
+
+
+
+
 /* 
 
 
