9a19bdd51b99ddc26d71b7c502dbefdd480be7b6
RedEyedFrog <50462492+Oloinen@users.noreply.github.com>
2019-12-16 16:04:44 +0200

    Viilausta

--- soveltoapp_reactfront/src/service/Request.js
+++ soveltoapp_reactfront/src/service/Request.js
@@ -9,6 +9,18 @@ export const fetchQuestions =(topic_id)=> {
   .then(res => res.json())
 };
 
+export const postQuiz = (quiz) => {
+  return fetch("api/topics/quiz", {
+    method: "POST",
+        headers: {
+        "Accept": "application/json", 
+        "Content-type": "application/json", 
+        "authorization": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6IlN1bGFra2FAc2lsYWtrYS5maSIsImlkIjoxLCJ0aW1lIjoiMjAxOS0xMi0xNlQxMTo0Mzo1OS43NThaIiwiaWF0IjoxNTc2NDk2NjM5LCJleHAiOjE1NzY1MTgyMzl9.DDL727Zu6GY_k0gWayPr0i42abIoVDRYdExl9pJ72BM'
+        },
+        body: JSON.stringify(quiz)
+    })
+  }
+
 
 
 
