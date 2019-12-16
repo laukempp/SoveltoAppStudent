ce4640b971e6b75fca5d4c797d27a3ab97b31cdc
laukempp <laukempp@gmail.com>
2019-12-16 14:39:15 +0200

    Questionform tehty perustoimivuuksineen. Vaatii vielä hienosäätöä ja kaiken css. GetTopics route lisätty backiin

---
+++ soveltoapp_reactfront/src/components/teacher/Questionform.js
@@ -0,0 +1,244 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import { postQuestion, getTopics } from "../../service/Request";
+import * as Yup from "yup";
+import auth from "../../service/Auth";
+
+const validationSchema = Yup.object().shape({
+  question: Yup.string()
+    .min(2, "Question must have a at least two characters")
+    .max(255, "Must be shorter than 255")
+    .required("This field is required"),
+  correct_answer: Yup.string()
+    .min(2, "Must have at least two characters")
+    .max(255, "Must be shorter than 255")
+    .required("This field is required"),
+  wrong_answer: Yup.string()
+    .min(2, "Must have at least two characters")
+    .max(255, "Must be shorter than 255")
+    .required("At least one is required")
+});
+
+export default function QuestionForm() {
+  const authT = auth.sessionStorageGetItem();
+
+  const [topics, setTopics] = useState([])
+  const fetchTopics = () => {
+    getTopics().then(res => setTopics(res))
+  }
+  useEffect(() => {
+    fetchTopics()
+  }, [])
+  console.log(topics);
+
+  let topicInput = topics.map(option => {
+    return (
+      <option value={option.id} label={option.title} />
+    )
+  })
+
+  if (authT) {
+    return (
+      <Formik
+        initialValues={{
+          question: "",
+          correct_answer: "",
+          wrong_answer: [""],
+          topics_id: 1
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          console.log(values);
+          postQuestion(values);
+          resetForm();
+          setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          handleReset
+        }) => (
+            <Form onSubmit={handleSubmit}>
+              <Field
+                as="select"
+                name="topics_id"
+                ClassName={touched.topics_id && errors.topics_id ? "error" : null}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.topics_id}
+                style={{ display: "block" }}
+              >
+                {topicInput}
+              </Field>
+              <Field
+                type="text"
+                name="question"
+                id="question"
+                placeholder="Enter the question here"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.question}
+                className={
+                  touched.question && errors.question ? "has-error" : null
+                }
+              />
+              <ErrorMessage
+                component="div"
+                name="question"
+                className="invalidQuestion"
+              />
+              <div>
+                <br />
+              </div>
+              <Field
+                type="text"
+                name="correct_answer"
+                id="correct_answer"
+                placeholder="Give correct answer here"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.correct_answer}
+                className={
+                  touched.correct_answer && errors.correct_answer
+                    ? "has-error"
+                    : null
+                }
+              />
+              <ErrorMessage
+                component="div"
+                name="correct_answer"
+                className="invalidCorrectAnswer"
+              />
+              <div>
+                <br />
+              </div>
+              <FieldArray name="wrong_answer"
+                render={({ insert, remove, push }) => (
+                  <div>
+                    {values.wrong_answer.length > 0 &&
+                      values.wrong_answer.map((one_wrong_answer, index) => (
+                        <div className="row" key={index}>
+                          <div className="col">
+                            <label htmlFor={`wrong_answer.${index}`}>Wrong Answer</label>
+                            <Field name={`wrong_answer.${index}`}
+
+                              placeholder="Add another"
+                              type="text" />
+                            <ErrorMessage
+                              component="div"
+                              name="wrong_answer"
+                              className="invalidWrongAnswer" />
+                          </div>
+                          <div className="col">
+                            <button
+                              type="button"
+                              className="secondary"
+                              onClick={() => remove(index)}>X
+                            </button>
+                          </div>
+                        </div>
+                      ))}
+                    <button type="button" className="secondary"
+                      onClick={() => push({ wrong_answer: "Add another" })}>
+                      Add another wrong answer
+                  </button>
+                  </div>
+                )}
+              />
+              <br />
+              <button onClick={event => {
+                event.preventDefault();
+                handleReset();
+              }}
+              >Reset</button>
+
+
+
+              <div className="input-row">
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+              </button>
+              </div>
+            </Form>
+          )}
+      </Formik>
+    );
+  } else {
+    return <Redirect to="/" />;
+  }
+}
+
+
+
+{/* <select
+                name="topic"
+                value={values.topics_id}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                style={{ display: 'block' }}
+              >
+                <option value={values.topics_id} label={values.topics_id} />
+                <option value="red" label="red" />>
+              </select> */}
+
+
+
+{/* <Field
+type="text"
+name="wrong_answer[0]"
+id="wrong_answer"
+placeholder="Give wrong answer here"
+onChange={handleChange}
+onBlur={handleBlur}
+value={values.wrong_answer[0]}
+className={
+  touched.wrong_answer && errors.wrong_answer ? "has-error" : null
+}
+/>
+<ErrorMessage
+component="div"
+name="wrong_answer[0]"
+className="iInvalidWrongAnswer"
+/>
+<Field
+type="text"
+name="wrong_answer[1]"
+id="wrong_answer"
+placeholder="Give wrong answer here"
+onChange={handleChange}
+onBlur={handleBlur}
+value={values.wrong_answer[1]}
+className={
+  touched.wrong_answer && errors.wrong_answer ? "has-error" : null
+}
+/>
+<ErrorMessage
+component="div"
+name="wrong_answer[1]"
+className="iInvalidWrongAnswer"
+/>
+<Field
+type="text"
+name="wrong_answer[2]"
+id="wrong_answer"
+placeholder="Give wrong answer here"
+onChange={handleChange}
+onBlur={handleBlur}
+value={values.wrong_answer[2]}
+className={
+  touched.wrong_answer && errors.wrong_answer ? "has-error" : null
+}
+/>
+<ErrorMessage
+component="div"
+name="wrong_answer"
+className="iInvalidWrongAnswer[2]"
+/> */}
--- soveltoapp_reactfront/src/service/Request.js
+++ soveltoapp_reactfront/src/service/Request.js
@@ -25,3 +25,30 @@ export const checkItem = () => {
 
 export const redirect = () => {};
  */
+const url = "/api/topics/";
+const token = sessionStorage.getItem("tommi");
+export const postQuestion = question => {
+  return fetch(`${url}/question`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", "Authorization": token },
+    body: JSON.stringify(question)
+  });
+};
+
+export const getTopics = topic => {
+  return fetch(`${url}/ids`).then(res => res.json())
+    .catch(err => err)
+}
+
+// export const deleteQuestion = id => {
+//   return fetch(`${url}/${id}`, {
+//     method: "DELETE"
+//   });
+// };
+
+// export const editQuestion = id => {
+//   return fetch(`${url}/${id}`, {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" }
+//   });
+// };
