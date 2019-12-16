e853be29e4188dd98e7c5766d94767ad09fc7c60
sormunen <sormunen.roni@gmail.com>
2019-12-16 16:15:14 +0200

    merge conflict incoming

--- soveltoapp_reactfront/src/components/teacher/Dashboard.js
+++ soveltoapp_reactfront/src/components/teacher/Dashboard.js
@@ -11,8 +11,8 @@ import Footer from '../../layout/Footer';
 export default function Dashboard() {
   const authT = auth.sessionStorageGetItem();
   
-  console.log("hello from dashboard", authT);
-  {authT ? null : <Redirect to="/" />}
+/*   console.log("hello from dashboard", authT);
+  {authT ? null : <Redirect to="/" />} */
 
   return (
     <div>
